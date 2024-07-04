848fd6cae42a32525fdaf9058db92bc1ad2e3ec2
kokwei <kokweikhong@gmail.com>
2024-07-04 12:31:08 +0800

    remove is prefix

--- src/app/admin/users/_components/update-button.tsx
+++ src/app/admin/users/_components/update-button.tsx
@@ -24,14 +24,14 @@ const UpdateButton: FC<UpdateButtonProps> = ({
   return (
     <div className="-mt-px flex divide-x divide-gray-200">
       <div className="flex flex-col gap-y-1 w-0 flex-1 p-2 items-center">
-        <h4 className="text-xs font-medium">isActive</h4>
+        <h4 className="text-xs font-medium">Active</h4>
         <Switch
           checked={isActive}
           onCheckedChange={(checked) => updateUserActiveStatus(userId, checked)}
         />
       </div>
       <div className="flex flex-col gap-y-1 w-0 flex-1 p-2 items-center">
-        <h4 className="text-xs font-medium">isApproved</h4>
+        <h4 className="text-xs font-medium">Approved</h4>
         <Switch
           checked={isApproved}
           onCheckedChange={(checked) =>
@@ -40,7 +40,7 @@ const UpdateButton: FC<UpdateButtonProps> = ({
         />
       </div>
       <div className="flex flex-col gap-y-1 w-0 flex-1 p-2 items-center">
-        <h4 className="text-xs font-medium">isVerified</h4>
+        <h4 className="text-xs font-medium">Verified</h4>
         <Switch
           checked={isVerified}
           onCheckedChange={(checked) =>
